5571fb544adb300b882cc203bd9b8373ba9861b2
Huynh <macbook@MacBook-cua-Tien-Tien.local>
2023-01-03 18:31:18 +0700

    Master: Adding my project file bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -5,14 +5,11 @@ import pandas as pd
 
 import numpy as np
 
-
-
 CITY_DATA = { 'chicago': 'chicago.csv',
 
               'new york city': 'new_york_city.csv',
 
               'washington': 'washington.csv' }
-
 
 
 def get_filters():
